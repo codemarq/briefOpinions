ae441f1c547060c5618e9013baa9da1809ef53fe
Eric <goodtuesday@gmail.com>
2016-09-11 17:24:25 -0400

    Add files via upload

--- assets/javascript/briefOpinionsfirebase.js
+++ assets/javascript/briefOpinionsfirebase.js
@@ -19,18 +19,24 @@ var recent = "";
 
 
 // Capture Button Click
-$("#search").on("click", function() {
+$("#search").on("click", function(event) {
 
-	// YOUR TASK!!!
-	// Code in the logic for storing and retrieving the most recent user.
-	// Dont forget to provide initial data to your Firebase database.
+event.preventDefault();
+
+console.log("database: ", database)
+
 
 	recent = $('#case-input').val().trim();
 
+console.log(recent)
+
+
 	database.ref().set({
 		recent: recent,
-	
 	});
+
+
+	
 
 	// Don't refresh the page!
 	return false;
@@ -39,13 +45,13 @@ $("#search").on("click", function() {
 //Firebase watcher + initial loader HINT: .on("value")
 database.ref().on("value", function(snapshot) {
 
+
 	// Log everything that's coming out of snapshot
 	console.log(snapshot.val());
-	console.log(snapshot.val().recent);
 	
 
 	// Change the HTML to reflect
-	$("#recentSearches").html(snapshot.val().recent);
+	$("#recentSearches").html(snapshot.val());
 
 
 
